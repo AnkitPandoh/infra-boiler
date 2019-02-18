3430ed2740011401080ed4bde19ca0fb364f20c4
Ankit Pandoh <ankpando@publicisgroupe.net>
2019-02-18 15:50:17 +0000

    first commit

---
+++ src/main/java/com/devops/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.devops.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView getHelloWorldView(){
+		return new ModelAndView("helloworld");
+	}
+}
